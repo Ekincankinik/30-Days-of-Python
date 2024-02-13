0b0ce6c357cb772202c60125b4cbc8edec85b0d5
Ekin <147428702+Ekincankinik@users.noreply.github.com>
2024-02-13 16:26:53 +0300

    Add files via upload

---
+++ Day 3 - Operators/day3.py
@@ -0,0 +1,152 @@
+# Exercises Day 3
+
+# Declare your age as integer variable
+# Declare your height as a float variable
+# Declare a variable that store a complex number
+
+1. 
+
+age = 100
+
+2. 
+
+height = float(192)
+
+
+
+3. 
+
+com_num = complex(1,2)
+
+# 4. 
+
+# base_of_triangle = int(input("Enter the base = "))
+# height_of_triangle = int(input( " Enter the height = "))
+# area_of_triangle = 0.5 * base_of_triangle * height_of_triangle
+
+# print(area_of_triangle)
+
+# 5. 
+
+
+# sideA = int(input("Enter the side A = "))
+# sideB = int(input("Enter the sideB = "))
+# sideC = int(input("Enter the sideC = "))
+# perimeterOfTriangle = sideA + sideB + sideC
+
+# print(perimeterOfTriangle)
+
+
+
+
+
+# Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+# Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+
+6. 
+
+# length_of_rectangle = int(input(" Enter the length = "))
+# width_of_rectangle = int(input(" Enter the width = "))
+# area_of_rectangle = length_of_rectangle * width_of_rectangle
+# print(area_of_rectangle)
+
+7. 
+# radius_of_circle = int(input(" Enter the radius =  "))
+# area_of_circle = 3.14 * radius_of_circle ** 2
+# print(area_of_circle)
+
+8. 
+
+
+# Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+
+12. 
+
+movie1 = 'python'
+movie2 = "dragon"
+
+len_of_movie1 = len(movie1)
+len_of_movie2 = len(movie2)
+
+print(len_of_movie1 != len_of_movie2)
+# or
+print( len(movie1) != len(movie2))
+
+13. 
+print('on' in 'python' and 'on' in 'dragon')
+
+14.
+
+sentence = " I hope this course is not full of jargon."
+print('jargon' in sentence)
+
+15. 
+print( 'on' not in 'python' and 'dragon')
+
+
+16. 
+
+float_len_of_movie1 = float(len_of_movie1)
+print(float_len_of_movie1)
+str_float_len_of_movie = str(float_len_of_movie1)
+print(str_float_len_of_movie)
+
+# or
+
+name = "dragon"
+print(str(float(len(name))))
+
+
+17. 
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in range(len(nums)):
+    if nums[i] % 2 == 0:
+        print(nums[i])
+   
+   
+# or
+        
+        
+# number =   int(input('Please enter a number to learn if it is odd or even = '))
+# if number % 2 == 0:
+#     print('The number is even')
+# else:
+#     print('The number is odd')
+
+18. 
+
+f_of_number = 7 // 2
+print(f_of_number)
+
+num1 = 2.7
+
+print(int(num1) == f_of_number)
+
+19. 
+
+
+print(type('10') == type(10))
+
+20. 
+print(int(9.8) == (10))
+
+21. 
+
+
+# workingHours = int(input('Enter the hours = '))
+# ratePerHour = int(input( 'Enter the rate per hour = '))
+# pay = workingHours * ratePerHour
+# print("Earning: ", pay,'$')
+
+22. 
+
+# totalYears = 100
+# yearsLived = int(input('Enter your age = '))
+# restOfSeconds =  (totalYears - yearsLived) * 365 * 12 * 60 * 60
+
+# print(restOfSeconds)
+
+23. 
+
